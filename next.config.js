5a1cfae92e580fa2ef92b3358754448e27f804a5
Brayden Meyer <123148100+brayden-meyer@users.noreply.github.com>
2023-06-20 08:13:40 -0400

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-subdomain",
+            value: "try",
+          },
+        ],
+        destination: "/demo/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
